8364b703e4bf7aa922733249d513b071809c26a6
nqtran1 <nqtran@ucsd.edu>
2020-10-31 18:09:53 -0700

    n

--- bot/testerbot.py
+++ bot/testerbot.py
@@ -45,10 +45,10 @@ async def question(ctx):
 #quizdata
 
 #Reads csv through file opposed to url
-import csv
+@client.event
 with open('quiz.csv', newline='') as q:
     reader = csv.reader(q)
     for row in reader:
-        print(row)
+        ctx.send(row)
         
 client.run('NzY4Mjg3MTgzMzY2MTI3NjY2.X4-RMg.sHXghTn0SIp6ubkrHGJz_SgFQ1s')
