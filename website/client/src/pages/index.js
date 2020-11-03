16e1993fab6ba17dca99450f4926e02633468048
ercpham <erc.pham@gmail.com>
2020-11-02 19:57:40 -0800

    Add routing between pages

---
+++ website/client/src/pages/index.js
@@ -0,0 +1,6 @@
+import QuizzesInsert from "./QuizzesInsert";
+import QuizzesList from "./QuizzesList";
+import QuizzesUpdate from "./QuizzesUpdate";
+import MainContent from "./MainContent";
+
+export { QuizzesUpdate, QuizzesList, QuizzesInsert, MainContent };
