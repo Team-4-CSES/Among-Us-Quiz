b0c5c74045fc7c9186e2cad8a0713c96bc7785f3
qa <derphrrp@gmai.com>
2020-11-15 16:38:36 -0800

    Made input format simpler

--- Hello There Bot/Discord Bot.py
+++ Hello There Bot/Discord Bot.py
@@ -9,12 +9,14 @@ import discord
 from discord.ext import commands
 import nest_asyncio
 import asyncio
+import os
 import cv2
 from PIL import Image
 from PIL import ImageColor
 from scipy import signal as sg
 import numpy as np
 import math
+import string, random
 import csv
 import re
 import time
@@ -28,6 +30,7 @@ token = tokenIn.readline()
 
 client = commands.Bot(command_prefix = '!')
 mongo = MongoClient(tokenIn.readline())
+db = mongo["quizInfo"]
 client.quiz = mongo.quizInfo.quizinfos
 client.elimination = None
 client.players = {}
@@ -123,90 +126,6 @@ async def on_message(message):
     reactions = message.reactions
     morse = False
     pic_ext = ['.jpg','.png','.jpeg']
-    if len(image)>0 and image[0].filename.endswith('.csv'):
-        await channel.send(embed = discord.Embed(title = "You have 10 seconds to react to the reaction below and join the game.", color = discord.Colour.blue()))
-        InvMsg = await channel.history().get(author__name='Hello There')
-        await InvMsg.add_reaction("💩")
-        time.sleep(10)
-        await channel.send("Starting")
-        print("reading csv")
-        await image[0].save('quiz.csv')
-        with open('quiz.csv', newline='') as q:
-            reader = csv.reader(q)
-            print(reader)
-            answer_dict = {'🇦': "A", '🇧': "B", '🇨': "C", '🇩': "D", 
-                           '🇪': "E", '🇫': "F", '🇬': "G", '🇭': "F", 
-                           '🇮': "I", '🇯': "J"}
-            for row in reader:
-                print(row)
-                if row[0] == "done":
-                    Final = discord.Embed(
-                        title = "Final Podium",
-                        
-                        color = discord.Colour.gold()
-                        )
-                    rank = 1
-                    medals = ["🥇", "🥈", "🥉"]
-                    for player in list(client.players.keys())[:3]:
-                        Final.add_field(name= medals[rank-1], value= player, inline=False)
-                        rank += 1
-                    await channel.send(embed=Final)
-                    client.players = {}
-                    break
-                def check(rxn, user):
-                    if user.name != "Hello There" and user.name in client.players.keys():
-                        return True
-                    else:
-                        return False
-                def equation(x):
-                    return 300-300*(x/(int(row[3])/1.5))**2
-                embed = discord.Embed(
-                    title = "Question " + row[0],
-                    description = row[1],
-                    
-                    colour = discord.Colour.blue()
-                )
-                emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯'] 
-                for i, e  in enumerate(emojis[:int(row[4])]):
-                    embed.add_field(name=e, value=row[5+i])                    
-                embed.add_field(name='Time:', value=row[3]+" seconds",inline=False)
-                await channel.send(embed = embed)
-                emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯'] 
-                msg = await channel.history().get(author__name='Hello There')
-                for emoji in emojis[:int(row[4])]:
-                    await msg.add_reaction(emoji)
-                answer = "Fail"
-                t0 = time.perf_counter()
-                try:
-                    answer = await client.wait_for("reaction_add", timeout=float(row[3]), check=check)
-                except:
-                    await channel.send("No Response Given")
-                if type(answer) != str:
-                    t1 = time.perf_counter()
-                    times = t1 - t0
-                    pts = equation(times)
-                    if pts < 10:
-                        pts = 10
-                    print("Time:", times)
-                    if answer[0].emoji in answer_dict.keys() and answer_dict[answer[0].emoji] == row[2]:
-                        await channel.send("Correct!  " + answer[1].name + " will be awarded " + str(int(round(pts, 0))) + " points.")
-                        client.players[answer[1].name] += int(round(pts, 0))
-                    else: 
-                        client.players.pop(answer[1].name, None)
-                        await channel.send("WRONG! The correct answer is " + row[2])
-                        await channel.send(answer[1].name + " will be kicked!")
-                client.players = dict(sorted(client.players.items(), key = lambda kv:kv[1], reverse=True))
-                print(client.players)
-                rankings = discord.Embed(
-                    title = "Rankings",
-                    
-                    color = discord.Colour.red()
-                    )
-                rank = 1
-                for player in client.players.keys():
-                    rankings.add_field(name= str(rank)+". "+player, value= str(client.players[player])+" point", inline=False)
-                    rank += 1
-                await channel.send(embed=rankings)
 
     for ext in pic_ext:
         if len(image) > 0 and image[0].filename.endswith(ext) and len(content) > 0:
@@ -287,7 +206,7 @@ async def run(message, Id):
             await channel.send(embed = discord.Embed(title = "You are playing by elimination", color = discord.Colour.blue()))
         else:
             await channel.send(embed = discord.Embed(title = "You are playing with score deductions", color = discord.Colour.blue()))
-        await channel.send("Starting")
+        await channel.send(embed = discord.Embed(title = "Starting", color = discord.Colour.green()))
         for iteration, row in enumerate(questions):
             if len(list(client.players.keys())) == 1 and client.elimination:
                 await channel.send(embed = discord.Embed(title = list(client.players.keys())[0] + " wins for being the last survivor!", color = discord.Colour.blue()))
@@ -299,7 +218,12 @@ async def run(message, Id):
                 podium.add_field(name= "🥇", value= list(client.players.keys())[0], inline=False)
                 await channel.send(embed = podium)
                 break
-            row = row.split("~")               
+            row = row.split("~")
+            for i in range(0, len(row)):
+                if row[i] == '':
+                    row[i] = False
+            for i in range(0, row.count(False)):
+                row.remove(False)
             def check(rxn, user):
                 if user.name != "Hello There" and user.name in client.players.keys():
                     return True
@@ -314,13 +238,17 @@ async def run(message, Id):
                 colour = discord.Colour.blue()
             )
             emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯'] 
-            for i, e  in enumerate(emojis[:int(row[4])]):
-                embed.add_field(name=e, value=row[5+i])                    
+            for i, e  in enumerate(emojis[:len(row[4:])]):
+                if row[4+i] == "TRUE":
+                    row[4+i] = "True"
+                elif row[4+i] == "FALSE":
+                    row[4+i] = "False"
+                embed.add_field(name=e, value=row[4+i])                    
             embed.add_field(name='Time:', value=row[3]+" seconds",inline=False)
             await channel.send(embed = embed)
             emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯'] 
             msg = await channel.history().get(author__name='Hello There')
-            for emoji in emojis[:int(row[4])]:
+            for emoji in emojis[:len(row[4:])]:
                 await msg.add_reaction(emoji)
             answer = "Fail"
             t0 = time.perf_counter()
@@ -358,6 +286,7 @@ async def run(message, Id):
                 rankings.add_field(name= str(rank)+". "+player, value= str(client.players[player])+" point", inline=False)
                 rank += 1
             await channel.send(embed=rankings)
+            time.sleep(2)
             if iteration == len(questions)-1:
                 Final = discord.Embed(
                     title = "Final Podium",
@@ -373,7 +302,94 @@ async def run(message, Id):
                 client.players = {}
                 break
     except:
-        await channel.send("Invalid Quiz Code Given")
+        await channel.send("Invalid Quiz Code Given or Invalid Quiz Set")
+
+@client.command()
+async def upload(ctx, filetype):
+
+    validfiletypes = ["url", "csv", "excel", "xls"]
+
+    #checks if parameter is good
+    filetypechecker = False
+    for i in range(len(validfiletypes)):
+        if validfiletypes[i] in filetype.lower():
+            filetypechecker = True
+
+    if not filetypechecker:
+        await ctx.send("Error! Unsupported file type!")
+        await ctx.send("The command's syntax goes as follows: !upload <filetype> <quiztype>.")
+
+    if filetype == "csv":
+
+        await ctx.send("Please upload your .CSV file.")
+
+        def check(message):
+            return message.attachments[0].filename.endswith('.csv') and message.author == ctx.author
+
+
+        # checks if the 4 letter id is unique. If not, creates a new one.
+
+        def quizcodemaker(col):
+            filename = ''.join(random.choice(string.ascii_uppercase) for i in range(4))
+            doc = col.find_one({"_id": "Key"})
+            codes = doc["Codes"]
+            for row in codes:
+                if filename in row:
+                    quizcodemaker()
+            return filename
+        
+        try:
+            message = await client.wait_for('message', timeout=10.0, check=check)
+            file = message.attachments
+            unique_quizcode = quizcodemaker(client.quiz)
+            ctx.send("survived this far!")
+            unique_filename = unique_quizcode + ".csv"
+
+            if len(file) > 0 and file[0].filename.endswith('.csv'):
+                await file[0].save(unique_filename)
+                with open(unique_filename, newline='') as q:
+                    reader = csv.reader(q, delimiter='~')
+                    for row in reader:
+                        await ctx.channel.send(row)
+                        
+            await ctx.channel.send("--------------")
+            await ctx.channel.send("Is this the quiz set you wish to create? (Y/N)")
+
+            def checkanswer(message):
+                return (message.content.lower() == "y" or message.content.lower() == "n") and message.channel == ctx.channel
+
+            try:
+                userAnswer = await client.wait_for('message', timeout=15.0, check=checkanswer)
+                print(userAnswer.content)
+                if userAnswer.content.lower() == "y":
+
+                    #inserts the code into the quiz key documents
+                    x = client.quiz.update({"_id": "Key"}, {'$addToSet': {"Codes": unique_quizcode}})
+
+                    client.quiz.insert_one({"_id": unique_quizcode, "questions": []})
+
+                    with open(unique_filename, newline='') as csvfile:
+                        reader = csv.reader(csvfile, delimiter=',')
+                        for row in reader:
+                            y = '~'.join(row)
+                            x = client.quiz.update({"_id": unique_quizcode}, {'$addToSet': {"questions": y}})
+
+                    await ctx.channel.send("Success! Your quiz set ID is " + unique_quizcode)
+                    os.remove(unique_filename)
+
+
+                elif userAnswer.content.lower() == "n":
+                    await ctx.channel.send("Got it... the quizset has been deleted.")
+                    os.remove(unique_filename)
+
+            except asyncio.TimeoutError:
+                await ctx.channel.send("You timed out!")
+                await ctx.channel.send("Please resend the command if you still wish to upload a quiz set.")
+                os.remove(unique_filename)
+
+        except asyncio.TimeoutError:
+            await ctx.channel.send("You timed out!")
+            await ctx.channel.send("Please resend the command if you still wish to upload a quiz set.")
     
 client.run(token)
     
