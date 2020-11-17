eb0a3ca56a3026687bbbd22cd4b648ad181e7ab3
Eric <erc.pham@gmail.com>
2020-11-17 12:27:23 -0800

    Changed ip address of server

--- website/api/index.js
+++ website/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
  const api = axios.create({
-   baseURL: "http://localhost:9000/api",
+   baseURL: "http://192.168.0.20:9000/api",
  });
 
 export const insertQuiz = (payload) => api.post(`/quiz`, payload);
